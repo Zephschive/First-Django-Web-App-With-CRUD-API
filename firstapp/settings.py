08f4f402f8965d2dc32eabbc15bc6bc9cc5fe985
Zephschive <zephaniahollennu@gmail.com>
2024-08-06 18:27:34 +0000

    committed

--- firstapp/settings.py
+++ firstapp/settings.py
@@ -34,12 +34,20 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground'
+    'playground',
+    'rest_framework', 
+    'corsheaders',
+    
 ]
 
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,10 +81,17 @@ WSGI_APPLICATION = 'firstapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zephtestdjango',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
 
--- playground/models.py
+++ playground/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 
+class Departments(models.Model):
+    DepartmentId = models.AutoField(primary_key=True)
+    DepartmentName = models.CharField(max_length=500)
+    
+class Employees(models.Model):
+    EmployeeId = models.AutoField(primary_key=True)
+    EmployeeName = models.CharField(max_length=500)    
+    Department = models.CharField(max_length=500)    
+    DateOfJoining = models.DateField()
+    PhotoFileName = models.CharField(max_length=500)
+    
